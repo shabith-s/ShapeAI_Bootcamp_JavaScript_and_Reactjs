ffbad5c49310937657898334c2603b12da37c4f0
shabith-s <63193595+shabith-s@users.noreply.github.com>
2021-06-26 20:56:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Headers from "./Header"
+import Footer from "./Footer"
+import Info from "./Info"
+
+
+function App(){
+  return(
+    <div>
+      <Headers />
+      <Footer />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
